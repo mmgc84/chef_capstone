97ccd59dc60617113c2a89e72c574e8930e145b2
Marcelo Gutierrez <mmgc84@gmail.com>
2021-07-22 10:41:38 -0600

    Added Tests and Recipe to Git cookbook

--- git/recipes/default.rb
+++ git/recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
+
+if platform_family?('debian')
+  apt_update
+  package 'git'
+end
+
+# if node['platform_family'] == 'debian'
+#   include_recipe 'apt'
+#   package 'git'
+# end
