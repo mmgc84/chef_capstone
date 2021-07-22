8410623d2cbe3d4a96d89165b0dc4fc639b6c315
Marcelo Gutierrez <mmgc84@gmail.com>
2021-07-22 12:34:49 -0600

    Added Tests and Recipe to install Atom cookbook

--- atom/recipes/default.rb
+++ atom/recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
+
+if platform_family?('debian')
+  apt_repository 'atom' do
+    key 'https://packagecloud.io/AtomEditor/atom/gpgkey'
+    uri 'https://packagecloud.io/AtomEditor/atom/any/'
+    arch 'amd64'
+    distribution 'any'
+    components ['main']
+  end
+  package 'atom'
+end
--- atom/test/integration/default/default_test.rb
+++ atom/test/integration/default/default_test.rb
@@ -1,16 +1,5 @@
-# InSpec test for recipe atom::default
-
-# The InSpec reference, with examples and extensive documentation, can be
-# found at https://docs.chef.io/inspec/resources/
-
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
+if os.debian?
+  describe package('atom') do
+    it { should be_installed }
   end
-end
-
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
 end
